61e1aea4bcc0fad72f9cf417230f1961721bbd1c
Daniel Serna <dan.serna312@gmail.com>
2025-03-06 12:52:09 -0500

    Fix: images path

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for littlelemon project.
 
@@ -121,11 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 # The settings for static files have been updated for the Graded assessment
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     "restaurant/static"
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
