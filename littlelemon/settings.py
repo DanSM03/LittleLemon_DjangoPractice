f788f33876d17407b27c18bd1060cdaa2dee4691
Daniel Serna <dan.serna312@gmail.com>
2025-03-06 13:35:28 -0500

    Fix: Routing of images

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -122,13 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 # The settings for static files have been updated for the Graded assessment
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "restaurant/static")
+    os.path.join(BASE_DIR, "restaurant", "static")
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
